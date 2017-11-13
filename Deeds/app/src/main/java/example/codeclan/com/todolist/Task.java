0faa0c8f1a310c670fd2591a0ddcaee6be6dcfde
Jamie Sime <jamiesime@gmail.com>
2017-11-13 03:34:48 +0000

    added crud functions, including converting data types in map function to convert them when they come back out of the db

--- Deeds/app/src/main/java/example/codeclan/com/todolist/Task.java
+++ Deeds/app/src/main/java/example/codeclan/com/todolist/Task.java
@@ -1,7 +1,13 @@
 package example.codeclan.com.todolist;
 
 
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
 import db.SqlRunner;
+
+import static android.R.attr.value;
+import static java.lang.String.valueOf;
 
 
 /**
@@ -94,11 +100,81 @@ public class Task {
         this.complete = complete;
     }
 
+    public static Priority priorityToEnum(String priority){
+        for (Priority value : Priority.values()){
+            if (value.getName().equals(priority)){
+                return Priority.valueOf(priority);
 
+            }
+        }
+        return Priority.LOW;
+    }
+
+    public static DeedType typeToDeedType(String type){
+        for (DeedType value : DeedType.values().){
+            if (value.getName().equals(type)){
+                return DeedType.valueOf(type);
+
+            }
+        }
+        return DeedType.TEST;
+    }
+
+    public static boolean completeToBool(String complete){
+        if (complete.equals("true")){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    
     public void save(){
         String sql = String.format("INSERT INTO tasks (title, due_date, details, priority, type, complete) VALUES ('%s', '%s', '%s', '%s', '%s', '%s')", this.title, this.getDate(), this.details, this.getPriority().getName(), this.getType().getName(), this.isCompleteToString());
         this.id = SqlRunner.executeUpdate(sql);
         SqlRunner.closeConnection();
     }
+
+    public void deleteAll(){
+        String sql = String.format("DELETE FROM tasks;");
+        SqlRunner.executeUpdate(sql);
+        SqlRunner.closeConnection();
+    }
+
+    public void delete(){
+        String sql = String.format("DELETE FROM tasks WHERE id = %d;", this.id);
+        SqlRunner.executeUpdate(sql);
+        SqlRunner.closeConnection();
+    }
+
+    public void update(){
+        String sql = String.format("UPDATE tasks SET (title = '%s', due_date = '%s', details = '%s', priority = '%s', type = '%s', complete = '%s') WHERE id = %d;", this.title, this.date, this.details, this.getPriority().getName(), this.getType().getName(), this.isCompleteToString(), this.id);
+        SqlRunner.executeUpdate(sql);
+        SqlRunner.closeConnection();
+    }
+
+    public static ArrayList<Task> map(ResultSet rs){
+        ArrayList<Task> tasks = new ArrayList<>();
+        try {
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String title = rs.getString("title");
+                String date = rs.getString("due_date");
+                String details = rs.getString("details");
+                String priority = rs.getString("priority");
+                String type = rs.getString("type");
+                String complete = rs.getString("complete");
+
+                tasks.add(new Task(title, date, details, priorityToEnum(priority), typeToDeedType(type), completeToBool(complete)));
+            }
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + " : " + e.getMessage());
+        } finally {
+            SqlRunner.closeConnection();
+        }
+
+        return tasks;
+    }
+
 
 }
